fdb526dcf93813a87f64eff579a5b1239dd100cc
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2023-11-13 18:23:40 +0100

    completed tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):  # alt option: admin.StackedInline -> less compact
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ["question_text", "pub_date", "display_most_voted_choice", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    # not included in tutorial, had this genius idea myself
+    def display_most_voted_choice(self, obj):
+        most_voted_choice = obj.choice_with_most_votes()
+        return most_voted_choice.choice_text if most_voted_choice else "No choices"
+
+    display_most_voted_choice.short_description = 'Choice with Most Votes'
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 import datetime
+from django.contrib import admin
 from django.utils import timezone
 
-# Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,8 +11,20 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+
+    # not included in tutorial I had this genius idea myself
+    def choice_with_most_votes(self):
+        mvc = self.choice_set.order_by('-votes').first()
+
+        return mvc
 
 
 class Choice(models.Model):
